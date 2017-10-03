d8338174a377f169d1bcbb1cfe9ea030d1ccf3c1
JRMcCoy <jamesrichard.mccoy@gmail.com>
2017-10-03 19:41:18 -0400

    Add files via upload

---
+++ LightApp/script.js
@@ -0,0 +1,36 @@
+//Var
+var on = true;
+var onButtonActiveColor = "#ff0000";
+var offButtonActiveColor = "#00ff00";
+var onButtonDisabledColor = "#000000";
+var offButtonDisabledColor = "#ffffff";
+
+
+
+function toggleButton(element){
+
+  var onButton =  document.getElementById("onButton");
+  var offButton =  document.getElementById("offButton");
+  var body = document.body;
+
+  if(on){
+    onButton.style.backgroundColor = onButtonDisabledColor;
+    offButton.style.backgroundColor = offButtonActiveColor;
+    onButton.style.color = "black";
+    offButton.style.color = "black";
+    body.style.backgroundColor = "#000000";
+    on = false;
+  }else{
+    onButton.style.backgroundColor = onButtonActiveColor;
+    offButton.style.backgroundColor = offButtonDisabledColor;
+    onButton.style.color = "black";
+    offButton.style.color = "white";
+    body.style.backgroundColor = "#ffffff";
+    on = true;
+  }/*
+  if(on){
+    alert("Lights on!")
+  }else{
+    alert("Lights off!");
+  }*/
+}

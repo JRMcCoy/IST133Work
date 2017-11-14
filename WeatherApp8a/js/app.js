c2c23d5e307ed5771ca6854bc51ff01a263d9d88
JRMcCoy <jamesrichard.mccoy@gmail.com>
2017-11-14 18:53:30 -0500

    Add files via upload

---
+++ WeatherApp8a/js/app.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+function MyApp() {
+    var version = "v1.0";
+    var widgetReference;
+    var widgetObject;
+
+    function setStatus(message) {
+        $("#app>footer").text(message);
+    }
+
+    this.start = function () {
+
+        widgetReference = $(WeatherWidget);
+        widgetObject = new WeatherWidget(widgetReference);
+
+        widgetReference.click(function () {
+            widgetObject.update();
+        });
+
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
+}
+
+
+$(function () {
+    window.app = new MyApp();
+    window.app.start();
+});

3119fa60b5a9378f836f801ff248ac7b65d7fe46
JRMcCoy <jamesrichard.mccoy@gmail.com>
2017-11-14 19:49:36 -0500

    Add files via upload

---
+++ WeatherApp8b/js/app.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+function MyApp() {
+    var version = "v1.0";
+    var widgetReference;
+    var widgetObject;
+
+    function setStatus(message) {
+        $("#app>footer").text(message);
+    }
+
+    this.start = function () {
+
+        widgetReference = $(WeatherWidget);
+        widgetObject = new WeatherWidget(widgetReference);
+
+        $("#getWeather").click(function () {
+            widgetObject.update();
+            getLocation();
+        });
+
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    $("#latitude").val(position.coords.latitude);
+                    $("#longitude").val(position.coords.longitude);
+                },
+                function (error) {
+                    $("#controls .error").text("ERROR: " + error.message).slideDown();
+                }
+            );
+        }   
+    }
+}
+
+
+$(function () {
+    window.app = new MyApp();
+    window.app.start();
+});

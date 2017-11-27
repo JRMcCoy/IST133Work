8c905405fef28013ca24415938b946899bb1b460
JRMcCoy <jamesrichard.mccoy@gmail.com>
2017-11-27 17:02:24 -0500

    Add files via upload

---
+++ WeatherApp8c/js/WeatherWidget.js
@@ -0,0 +1,56 @@
+﻿function WeatherWidget($widget) {
+
+    this.update = function (locationInfo) {
+        $(".results", $widget).hide();
+        $(".loading", $widget).show();
+        getWeatherReport(locationInfo);
+    };
+
+    function getWeatherReport(locationInfo) {
+		if (locationInfo == "") return;
+		var apiUrl = "https://api.weather.gov/points/" + locationInfo;
+		$.ajax({
+			url: apiUrl,
+			dataType: "json"
+		})
+		.done(function(data) { displayMetadata(data); })
+		.fail(function(jqXHR, textStatus, errorThrown) { handleAjaxError(errorThrown); });
+		
+		apiUrl += "/forecast";
+		$.ajax({
+			url: apiUrl,
+			dataType: "json"
+		})
+		.done(function(data) { displayForecast(data); })
+		.fail(function(jqXHR, textStatus, errorThrown) { handleAjaxError(errorThrown); });
+	}
+
+	function displayMetadata(data) {
+		$("#loc").text(data.properties.relativeLocation.properties.city + data.properties.relativeLocation.properties.state);
+	}
+	
+	function displayForecast(data) {
+		populateWeather(data);
+	}
+	
+	function handleAjaxError(error) {
+		alert(error);
+	}
+
+    function populateWeather(data) {
+
+        var observation = data.properties.periods[0];
+
+        $(".results header img", $widget).attr("src", data.properties.periods[0].icon);
+		
+		$("#conditions").text(observation.shortForecast);
+		
+		$("#temp").text(observation.temperature + observation.temperatureUnit + " " + observation.temperatureTrend);
+		
+		$("#wind").text(observation.windSpeed + " " + observation.windDirection);
+		
+        $(".loading", $widget).fadeOut(function () {
+            $(".results", $widget).fadeIn();
+        });
+    }
+}
---
+++ WeatherApp8c/js/app.js
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+function MyApp() {
+    var version = "v3.0";
+    var widgetReference;
+    var widgetObject;
+	var locationInfo = "";
+	
+
+    function setStatus(message) {
+        $("#app>footer").text(message);
+    }
+
+    this.start = function () {
+
+		getLocation();
+	
+        widgetReference = $(WeatherWidget);
+        widgetObject = new WeatherWidget(widgetReference);
+
+        $("#getWeather").click(function () {
+            widgetObject.update(locationInfo);
+        });
+
+        $("#app>header").append(version);
+        setStatus("ready");
+    };
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+					locationInfo = position.coords.latitude + "," + position.coords.longitude;
+                    $("#latitude").val(position.coords.latitude);
+                    $("#longitude").val(position.coords.longitude);
+                },
+                function (error) {
+                    $("#controls .error").text("ERROR: " + error.message).slideDown();
+                }
+            );
+        }   
+    }
+}
+
+
+$(function () {
+    window.app = new MyApp();
+    window.app.start();
+});
